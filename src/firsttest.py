cb68e6a034eb9b52d4b71dbdb65dae485fdca90c
Ashish <ashish.p.deshmukh@gmail.com>
2017-07-20 18:15:58 +0530

    Initial commit

---
+++ src/firsttest.py
@@ -0,0 +1,23 @@
+print ("Hello world")
+
+myint = 7
+print(myint)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+string = "hello"
+strin2 = "world"
+helloworld = string + " " +strin2
+print(helloworld)
+
+list = [1,2,3,4,5,6]
+print(list[2])
+
+for x in list:
+    print(x)
+
+number = 1+2*3/4
+print(number)
